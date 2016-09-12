ab8095bf7175147d0245ba07e0511d272024e526
Christian Vivas Santiago <christian130@gmail.com>
2016-09-12 16:34:16 -0400

    procediendo con la funcionalidad CRUD, creado el DAO de las cuatro tablas

---
+++ src/main/java/conexus/IndexController.java
@@ -0,0 +1,14 @@
+package conexus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
+public class IndexController {
+	@RequestMapping("/login")
+    public String login(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "login";
+    }
+
+}
