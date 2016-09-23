4dd2f36b5bd93f882da0de7fb4309db817077a58
Christian Vivas Santiago <christian130@gmail.com>
2016-09-22 20:37:40 -0400

    sucessful integration with dataSource using GenericXmlApplicationContext, the context-application.xml and the jdbc.properties, some fixes in the DAO 1.- i dont know if the class CitasMedicasImplDAO.java should wxist in the implDAO instead of moving out... thinking 2.- planning to move the code and eliminate this CitasMedigasImpl is not implementing the corresponding interfaces

--- src/main/java/conexus/Application.java
+++ src/main/java/conexus/Application.java
@@ -1,12 +1,9 @@
 package conexus;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 
 
 
@@ -16,15 +13,9 @@ public class Application extends SpringBootServletInitializer {
 	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 	        return application.sources(Application.class);
 	    }
-	 @Autowired
-		private ApplicationContext appContext;
     public static void main(String[] args) {
-    	ApplicationContext context = new GenericXmlApplicationContext("application-context.xml");
-    	
-    	//context.getBean("dataSource", Singleton.class);
-        //SpringApplication.run(Application.class, args);
-        
-        System.out.println("sss"+ context.getBeanDefinitionNames());
+        SpringApplication.run(Application.class, args);
+             
     }
     }
    
--- src/main/java/implDAO/CitasMedicasImplDAO.java
+++ src/main/java/implDAO/CitasMedicasImplDAO.java
@@ -12,38 +12,27 @@ import javax.swing.table.DefaultTableModel;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import conexusDTO.UsuarioDTO;
+import oracle.jdbc.pool.OracleDataSource;
 
-public class CitasMedicasImplDAO implements ApplicationContextAware  {
-   static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";  
-   static final String DB_URL = "jdbc:oracle:thin:@//192.168.56.101:1521/SERVICENAMEjdbc:mysql://localhost/brasilia_diario";
-   static final String USER = "root";
-   static final String PASS = "";
+public class CitasMedicasImplDAO  {
+
    
-   Connection conn = null;
+	private Connection myConnection = null;
+	private ApplicationContext context;
    public CitasMedicasImplDAO() {   
-   /*try{
-      Class.forName("oracle.jdbc.driver.OracleDriver");
-      //System.out.println("Connecting to database...");
-      conn = DriverManager.getConnection(DB_URL,USER,PASS);
-   }catch(SQLException se){
-      se.printStackTrace();
-   }catch(Exception e){
-      e.printStackTrace();
-   }*/
+ 
    
 }
   public boolean getLogin(UsuarioDTO objUsuarioDTO) throws Exception {
 	  
 	  try {
-		  
-          String dbURL = "jdbc:oracle:thin:@192.168.56.101:1521:deasrrollo";
-          String strUserID = "CITAS_MEDICAS";
-          String strPassword = "123456";
-          Connection myConnection=DriverManager.getConnection(dbURL,strUserID,strPassword);
-
-          Statement sqlStatement = myConnection.createStatement();
+        ApplicationContext context = new GenericXmlApplicationContext("application-context.xml");
+        OracleDataSource dataSource=  (OracleDataSource) context.getBean("dataSource");
+      	Connection myConnection = dataSource.getConnection();
+         Statement sqlStatement = myConnection.createStatement();
           String readRecordSQL = "select * from USUARIO_K78 where ROWNUM < 2 and ROWNUM >0";
           //String readRecordSQL = "select * from USUARIO_K78 where ROWNUM < 2 and ROWNUM >0 and LOGIN like '%"+objUsuarioDTO.getLogin()+"%' and CLAVE like '%"+objUsuarioDTO.getPasssha256()+"%'";  
           ResultSet myResultSet = sqlStatement.executeQuery(readRecordSQL);
@@ -59,84 +48,10 @@ public class CitasMedicasImplDAO implements ApplicationContextAware  {
       }
 	
 	  return false;
-	  /*Statement stmt = null;
-	  try {
-		stmt = conn.createStatement();
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-		return false;
-	}
-      String sql;
-      sql = "SELECT * FROM `tbl_usuarios` where `tbl_usuarios`.`alias` like '"+usuario.toString()+"' and `tbl_usuarios`.`clave` like '"+clave.toString()+"' ";
-      try {
-		ResultSet rs = stmt.executeQuery(sql);
-		if(rs.next()){
-			return true;
-			}else{
-				System.out.println(sql);
-				//System.out.println("no");
-			}
 	
-		
-		 //if(stmt!=null)
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-		return false;
-	}
-	return false;
-	 */ 
 	  
   }
-   public void close(){
-	   try {
-		conn.close();
-		System.out.println("closing connection");
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-   }
+  
    
-   public void RellenarGrid(JTable table, String Query)
-   {
-       try
-       {
-           //modelo mod = new modelo();
-           //CreateConnection();
-           Statement stat = conn.createStatement();
-           ResultSet rs = stat.executeQuery(Query);
-
-           //To remove previously added rows
-           while(table.getRowCount() > 0) 
-           {
-               ((DefaultTableModel) table.getModel()).removeRow(0);
-           }
-           int columns = rs.getMetaData().getColumnCount();
-           while(rs.next())
-           {  
-               Object[] row = new Object[columns];
-               for (int i = 1; i <= columns; i++)
-               {  
-                   row[i - 1] = rs.getObject(i);
-               }
-               ((DefaultTableModel) table.getModel()).insertRow(rs.getRow()-1,row);
-           }
-
-           rs.close();
-           stat.close();
-           conn.close();
-       }
-       catch( SQLException e)
-       {
-       }
-   }
-   private ApplicationContext applicationContext;
-@Override
-public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-	this.applicationContext = applicationContext;
-	// TODO Auto-generated method stub
-	
-}
+ 
 }
